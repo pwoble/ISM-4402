2a2e4a148ea0c22f0fe959550a8e07276e1d74ae
pwoble <54560953+pwoble@users.noreply.github.com>
2019-09-16 17:18:44 -0400

    Add files via upload

---
+++ Woble_2.1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+import numpy as np
+import glob
+
+all_data = pd.DataFrame()
+for f in glob.glob("Datasets/weekdata*.xlsx"):
+    df = pd.read_excel(f)
+    all_data = all_data.append(df,ignore_index=True)
+all_data.describe()
+
+
+# In[ ]:
+
+
+
+
