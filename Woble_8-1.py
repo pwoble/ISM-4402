70c17feef152b59cd36f903f5a99eb85793a6479
pwoble <54560953+pwoble@users.noreply.github.com>
2019-10-25 12:58:26 -0400

    Add files via upload

---
+++ Woble_8-1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+get_ipython().magic(u'matplotlib inline')
+Location = "datasets/gradedata.csv"
+df = pd.read_csv(Location)
+df.head()
+
+
+# In[2]:
+
+
+df.hist()
+
+
+# In[3]:
+
+
+df.hist(column="age", by="gender")
+
+
+# In[4]:
+
+
+df.hist(column="hours", by="gender")
+
+
+# In[5]:
+
+
+df.hist(column="age", by="gender")
+
+
+# In[ ]:
+
+
+
+
